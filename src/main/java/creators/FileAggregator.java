085ad359119dfa126aa38b84ce420db6fdc4d89e
gmittmann <gesamittmann@aol.com>
2019-04-09 16:58:28 +0200

    already existing aggregate file does not lead to failure anymore

--- src/main/java/creators/FileAggregator.java
+++ src/main/java/creators/FileAggregator.java
@@ -6,27 +6,30 @@ import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.project.MavenProject;
 
 public class FileAggregator {
-	
+
 	private MavenProject project;
 	private Log log;
-	
-	public FileAggregator (MavenProject project, Log log) {
+
+	public FileAggregator(MavenProject project, Log log) {
 		this.project = project;
 		this.log = log;
 	}
-	
+
 	public void aggregateFilesTo(File path, String fileName) {
-		
+
 		log.info("WILL DO SOMETHING, SOON");
-		
+
 		FileWriter out = new FileWriter(log);
 		log.info(path + "\\" + fileName);
-		out.createFile(path + "\\" + fileName);
-		out.writeIntoFile("tada");
-		out.finishFile();
-		
-		
-		//TODO: positioning of the file into target of main pom or plugin defined location
+		if (out.createFile(path + "\\" + fileName)) {
+			out.writeIntoFile("tada");
+			out.finishFile();
+		} else {
+			log.error("File already exists. Please delete the old file first"); //TODO: better handling
+		}
+
+		// TODO: positioning of the file into target of main pom or plugin defined
+		// location
 //		String filePath = path;
 //		if(!project.isExecutionRoot()) {
 //			file
@@ -38,9 +41,10 @@ public class FileAggregator {
 //		}
 //		
 //		log.info("put file to: " + file.getAbsolutePath());
-		
-		//TODO: check if all modules created a file -> warn if there is one, that did not
-		//TODO: put all information in one file
+
+		// TODO: check if all modules created a file -> warn if there is one, that did
+		// not
+		// TODO: put all information in one file
 
 	}
 
